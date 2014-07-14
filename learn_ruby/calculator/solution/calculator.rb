8d831088d967ed6bb5f480ffc9ad290f4fd9de19
K-S-A <atillas@ukr.net>
2014-07-14 22:14:28 +0300

    Update calculator.rb
    Added: multiply, power.
    Changed: factorial.

--- learn_ruby/calculator/solution/calculator.rb
+++ learn_ruby/calculator/solution/calculator.rb
@@ -10,10 +10,14 @@ def sum(numbers)
   numbers.inject(0){|total, number| total+number}
 end
 
-def factorial x
-  if x <= 1
-    1
-  else
-    x * factorial(x-1)
-  end
+def multiply(numbers)
+    numbers.reduce(1, :*)
+end
+
+def power(x, y)
+    x**y
+end
+def factorial(x)
+    (1..n).reduce(1, :*)
+end
 end
