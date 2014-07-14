3a16fc97fca600b72880cf7b3113614667e9d8ee
K-S-A <atillas@ukr.net>
2014-07-14 23:05:13 +0300

    Update timer.rb
    Added: padded.

--- learn_ruby/timer/solution/timer.rb
+++ learn_ruby/timer/solution/timer.rb
@@ -11,6 +11,10 @@ class Timer
     remainder = @seconds%3600
     sprintf("%02d:%02d:%02d", hours, remainder/60, remainder%60)
   end
-  
+  def padded(n)
+      n < 10 ? a = 0 : a = n.div(10)
+      b = n % 10
+      a.to_s + b.to_s
+  end
 end
 
